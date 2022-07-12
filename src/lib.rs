8573310c64f3244ca674b315d98470c0dd4be586
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-07-12 14:53:27 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ fn gamma_function(a: f64) -> f64
   // In Stan's Library, Ed.S.Sykora, Vol.II. First released December 28, 2008.
   // Link: http://www.ebyte.it/library/docs/math08/GammaApproximationUpdate.html.
   // See Nemes' formula & Fig.1 on page 6 of full text: Nemes_6.
-  // Application: Unconventional natural gas production decline analysis
+  // Application: Unconventional natural gas production decline analysis (prediction and history-matching)
   let  coefficient6 = ( 1.0 + 1.0/(12.0 * a * a) + 1.0/(1440.0 * a.powf(4.0)) + 239.0/(362880.0 * a.powf(6.0)) ).powf(a);   //Nemes_6 coefficient
   return (a/E).powf(a) * (2.0 * PI / a).sqrt() * coefficient6;
 }
@@ -33,7 +33,7 @@ fn  gdf(a: f64, x: f64) -> f64
   // A function for calculating gamma distribution function
   // Reference: NIST/SEMATECH e-Handbook of statistical methods.
   // http://www.itl.nist.gov/div898/handbook/eda/section3/eda366b.htm. Retrieved January 5, 2016.
-  // Application: Unconventional natural gas production decline analysis
+  // Application: Unconventional natural gas production decline analysis (prediction and history-matching)
   let gdf_value =  a.powf(x - 1.0) * (-a).exp() / gamma_function(x);
   println!("Gamma Distribution Function value is: {gdf_value}");
   return  gdf_value;
