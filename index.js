097d14f74d5554dae7000f4280256baf25ada408
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-07-11 20:21:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-//
+/****************************************************************************************************************************************
+# * index.js                                                                                                                          *
+#  **************************************************************************************************************************************
+#  *                                                                                                                                    *
+#  * @License Starts                                                                                                                    *
+#  *                                                                                                                                    *
+#  * Copyright © 2015 - present. MongoExpUser.  All Rights Reserved.                                                                    *
+#  *                                                                                                                                    *
+#  * License: MIT - https://github.com/MongoExpUser/Debian-Based-NAPI-Rust-Addons/blob/main/LICENSE                                     *
+#  *                                                                                                                                    *
+#  * @License Ends                                                                                                                      *
+#  **************************************************************************************************************************************
+# *                                                                                                                                     *
+# *  Project: Rust Container Project for NAPI-Rust                                                                                      *
+# *                                                                                                                                     *
+# *  This index.js file creates a simple class to test:                                                                                 *
+# *                                                                                                                                     *                                                                                                              *
+# *   1)  Invocation of NAPI-Rust Addon modules from within Node.js                                                                     *
+# *                                                                                                                                     *
+# *                                                                                                                                     *
+# ***************************************************************************************************************************************/
+
+
+
+class OilApiGravity
+{
+    constructor()
+    {
+      return null;
+    }
+    
+    oilApiGravity(specificGravity)
+    {
+      const { api } = require("./api.node");
+      return api(specificGravity);
+    }
+}
+
+
+(async function test()
+{
+  const oag = new  OilApiGravity();
+  oag.oilApiGravity(1.02);
+ 
+})();
