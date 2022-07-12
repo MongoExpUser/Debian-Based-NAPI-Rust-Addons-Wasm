aa931da218f6fb9acba4fa709fc0cac4c65c67fe
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-07-11 20:16:57 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 use napi_derive::napi;
 
 #[napi]
-fn oil_api_gravity(specific_gravity: f64) -> u8
+fn api(specific_gravity: f64) -> u8
 {
-   let api = (141.5/specific_gravity) - 131.5;
-   let oil_api_gr = api as u8;
-   println!("Oil API Gravity is: {oil_api_gr}");
-   return oil_api_gr;
+   let api_value = (141.5/specific_gravity) - 131.5;
+   let oil_api_gravity = api_value as u8;
+   println!("Oil API Gravity is: {oil_api_gravity}");
+   return  oil_api_gravity;
 }
