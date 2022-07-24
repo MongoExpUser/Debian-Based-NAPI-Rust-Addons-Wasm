44253d005e12e36f2a0241da6e5dec284240cc88
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-07-24 18:42:28 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 // main.rs 
 
+mod database;
+pub use crate::database::credentials;
+
 fn main()
 {
-  println!("Testing NAPI-Rust Addons");
+    // create postgres database credential
+    let postgres_username: String  = "postgres".to_string();
+    let postgres_password_length: usize  = 10;
+    credentials::database_credentials(postgres_username, postgres_password_length);
+    
+    // confirm successfull test
+    println!("Testing NAPI-Rust Addons");
 }
-
