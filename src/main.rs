d916f1c49069b8466bb636fbd0789b898e7d7fb3
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2022-07-24 20:44:57 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 // main.rs 
 
-mod database;
-pub use crate::database::credentials;
+mod utilities;
+pub use crate::utilities::utils;
 
 fn main()
 {
     // create postgres database credential
     let postgres_username: String  = "postgres".to_string();
     let postgres_password_length: usize  = 10;
-    credentials::database_credentials(postgres_username, postgres_password_length);
+    utils::database_credentials(postgres_username, postgres_password_length);
     
     // run commands via processes
     let cwd_one: String = "/home/ubuntu".to_string();
@@ -17,7 +17,7 @@ fn main()
     let cmd_two = "ls".to_string();
     let mut args_one:  [String; 2] = [ "-l".to_string(), "-a".to_string() ];
     let mut args_two:  [String; 1] = [ "-trl".to_string() ];
-    credentials::process_builder_command(cwd_one, cwd_two, cmd_one, cmd_two, &mut args_one, &mut args_two);
+    utils::process_builder_command(cwd_one, cwd_two, cmd_one, cmd_two, &mut args_one, &mut args_two);
     
     // confirm successfull test
     println!("Testing NAPI-Rust Addons");
